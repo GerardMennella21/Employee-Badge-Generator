4ea3e52288af9c8ed9bb772c18b6676b9fb22638
Gerard Mennella <gerardmennella21@gmail.com>
2023-03-08 10:41:07 -0500

    push to github

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CatWorx.BadgeMaker
+{
+    class Program
+    {
+        async static Task Main(string[] args)
+        {
+            Console.WriteLine("Would you like to create custom badges? (enter yes or no)");
+            string option = Console.ReadLine() ?? "";
+            if (option == "yes") {
+                List<Employee> employees = PeopleFetcher.GetEmployees();
+                Util.PrintEmployees(employees);
+                Util.MakeCSV(employees);
+                await Util.MakeBadges(employees);
+            } else if (option == "no") {
+                List<Employee> employees = await PeopleFetcher.GetFromApi();
+                Util.PrintEmployees(employees);
+                Util.MakeCSV(employees);
+                await Util.MakeBadges(employees);
+            } else {
+                Console.WriteLine("Invalid response!");
+            }      
+        }
+    }
+}
